2ae0f055368de465d44975e773bc7c46638524f6
rafaelferres <raferres@outlook.com.br>
2020-06-11 14:57:43 -0300

    Add Procfile

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
 import { App } from './app';
 
 const app = new App();
-//app.run();
-/*import puppeteer from "puppeteer";
-
-async function run(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on('request', (interceptedRequest: any) => {
-        if(interceptedRequest.url().includes("!ut/p/a1")){
-            console.log(interceptedRequest.url());
-        }
-        interceptedRequest.continue();
-      });
-
-    await page.goto('http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena');
-    await browser.close();
-}
-
-run();*/
